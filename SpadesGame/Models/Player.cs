b2f56e1623734c5a5faa66cbe91c21a1d8c000a8
Fabio Veiga <droidbinho@gmail.com>
2020-05-27 20:45:40 -0300

    Created Player Class

---
+++ SpadesGame/Models/Player.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpadesGame.Models
+{
+    public class Player
+    {
+        public int IdPlayer { get; set; }
+        public string NamePlayer { get; set; }
+        public List<Card> Hand = new List<Card>();
+
+        public Player() { }
+    }
+}
