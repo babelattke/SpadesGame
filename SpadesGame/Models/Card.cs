17e663bbf6dc2c309243b49828c847723bd25470
Fabio Veiga <droidbinho@gmail.com>
2020-05-27 20:43:45 -0300

    Created Card Class

---
+++ SpadesGame/Models/Card.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpadesGame.Models
+{
+    public class Card
+    {
+        public string Suit { get; set; }
+        public string IdCard { get; set; }
+        public int valueCard { get; set; }
+        public int Priority { get; set; }
+
+        public Card() { }
+    }
+}
